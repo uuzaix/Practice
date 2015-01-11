f508c19d33513a043612141d9cc8c27a97757239
Larisa Dolgova <larisa.dolgova@gmail.com>
2015-01-11 21:18:03 +0300

    add checker function

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,12 +1,15 @@
+def checker(i):
+	if i%3 == 0 and i%5 == 0:
+		print "FizzBuzz"
+	elif i%3 == 0:				
+		print "Fizz"
+	elif i%5 == 0:				
+		print "Buzz"
+	else:
+		print i
+
 def counter():
 	for i in range(1,101):
-		if i%3 == 0 and i%5 == 0:
-			print "FizzBuzz"
-		elif i%3 == 0:				
-			print "Fizz"
-		elif i%5 == 0:				
-			print "Buzz"
-		else:
-			print i
-	return
+		checker(i)
+		
 counter() 
