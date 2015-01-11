2b31a310444ee4fb88a503b7b322432246f335e1
Larisa Dolgova <larisa.dolgova@gmail.com>
2015-01-11 20:20:09 +0300

    add function to Oleg_FizzBuzz

--- Oleg_Fizzbuzz.py
+++ Oleg_Fizzbuzz.py
@@ -1,11 +1,13 @@
-for n in range (1,101):
-	if n%3 ==0 and n%5 == 0:
-		print 'FizzBuzz'
-	else:
-		if n%3 == 0:
-			print 'Fizz'
+def FizzBuzzer():
+	for n in range (1,101):
+		if n%3 ==0 and n%5 == 0:
+			print 'FizzBuzz'
 		else:
-			if n%5 == 0:
-				print 'Buzz' 
+			if n%3 == 0:
+				print 'Fizz'
 			else:
-				print n 
+				if n%5 == 0:
+					print 'Buzz' 
+				else:
+					print n
+FizzBuzzer() 
