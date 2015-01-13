14a70152e177658533bb52378f169137aaa7ddff
Larisa Dolgova <larisa.dolgova@gmail.com>
2015-01-14 00:19:28 +0300

    add test function

--- Words_Inverter_Oleg.py
+++ Words_Inverter_Oleg.py
@@ -1,6 +1,19 @@
-def invert_words_in(S):
-	words_list = S.split()
+def invert_words(s):
+	words_list = s.split()
 	words_list.reverse()
 	result = ' '.join(words_list)
-	print result
+	return result
 
+def test(actual, expected):
+	if actual == expected:
+		print "Test passed"
+	else:
+		print "Test failed: expected: %s, got: %s" % (expected, actual)
+
+def test_invert_words():
+	test (invert_words('I am a cat'), 'cat a am I')
+	test (invert_words('I am a cat'), 'I ma a tac')
+	test (invert_words(''), '')
+	
+	
+test_invert_words()
