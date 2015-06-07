a2029fb800d81ecb9a926fca58bcac3a8f16bf24
Larisa Dolgova <larisa.dolgova@gmail.com>
2015-06-07 23:34:23 +0300

    Some fixes

--- my_server2.py
+++ my_server2.py
@@ -15,23 +15,27 @@ sock.listen(1)
 # Wait for a connection
 print 'waiting for a connection'
 connection, client_address = sock.accept()
-
 print 'connection from', client_address
 
+# Read from socket
 data = connection.makefile()
+status = data.readline()
 
+# Check the request and remember the path
+if status[:3] == 'GET':
+	list_data = status.split()
+	path = list_data[1]
+	message = "Hello, %s" % path
+else:
+	raise Exception("bad request")
+
+# Find the end of request
 for line in data:
-	if line != '\r\n':
-		#print line
-		if line[:3] == 'GET':
-			list_data = line.split()
-			path = list_data[1]
-			message = 'HTTP/1.1 200 OK\n' + '\n' +"Hello, %s" % path
-			#print message			
-		
-	else:
-		#print "1"		
-		connection.sendall(message)
+	if line == '\r\n':
 		break
+
+#Send the message back
+data.writelines(['HTTP/1.1 200 OK\n', '\n', message])
+
 connection.close()
 
