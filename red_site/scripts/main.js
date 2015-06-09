36a15b42af472735c6d766be37da74efcfe24346
Larisa Dolgova <larisa.dolgova@gmail.com>
2015-06-09 23:42:45 +0300

    goodlooking working version

--- red_site/scripts/main.js
+++ red_site/scripts/main.js
@@ -2,12 +2,12 @@ var text = document.querySelector('p');
 var checkbox = document.getElementById('checkbox');
 
 checkbox.onclick = function (){
-if (checkbox.checked){
-text.setAttribute("class", 'redText');
-}
-else{
-text.setAttribute("class", 'blackText');
-}
+	if (checkbox.checked){
+		text.setAttribute("class", 'redText');
+	}
+	else{
+		text.setAttribute("class", 'blackText');
+	}
 }
 
 
