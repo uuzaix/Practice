10f3d551460a8785517468189d00c3d32ef06cee
Larisa Dolgova <larisa.dolgova@gmail.com>
2015-06-15 22:19:51 +0300

    ToDo with checkboxes

--- todo/scripts/main.js
+++ todo/scripts/main.js
@@ -1,18 +1,26 @@
-
 var button =  document.getElementById('button')
 var list = document.getElementById('todo-list')
 
 function addToList() {
 	var input = document.getElementById('input').value;
 	var output = document.createElement('li');
+
+	var checkBox = document.createElement("input");
+	checkBox.type = 'checkbox';
+	checkBox.id = 'c1';
+	checkBox.value = name;
+	checkBox.checked = false;
+
 	output.appendChild(document.createTextNode(input)); 
+	list.appendChild(checkBox);
 	list.appendChild(output);
 	console.log(input);
+	document.getElementById('input').value = null;
 }
 
 button.onclick = addToList;
 
-document.onkeydown	= function(){
+document.onkeydown    = function(){
 	if (window.event.keyCode == '13'){
 		addToList();
 	}
