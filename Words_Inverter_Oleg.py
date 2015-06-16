d76bfc3abf8850e66aa7a641a05b4edf2c5d6cfb
Larisa Dolgova <larisa.dolgova@gmail.com>
2015-06-16 22:59:22 +0300

    Commited old changes

--- Words_Inverter_Oleg.py
+++ Words_Inverter_Oleg.py
@@ -1,19 +1,39 @@
-def invert_words(s):
+#def invert_words(s):
+	#words_list = s.split()
+	#words_list.reverse()
+	#result = ' '.join(words_list)
+	#return result
+
+def invert_letters(s):
 	words_list = s.split()
-	words_list.reverse()
-	result = ' '.join(words_list)
-	return result
+	print words_list
+	inverted_words = []
+	for word in words_list:
+		index = 0
+		single_word = []
+		for letter in word:
+			a = len(word)-1-index
+			single_word.insert (a, letter)
+			print single_word
+			index = index + 1
+			inverted_words.append (single_word)
+	print inverted_words
+			
+	#result = ' '.join(words_list)
+	#return result
 
-def test(actual, expected):
-	if actual == expected:
-		print "Test passed"
-	else:
-		print "Test failed: expected: %s, got: %s" % (expected, actual)
+#def test(actual, expected):
+	#if actual == expected:
+		#print "Test passed"
+	#else:
+		#print "Test failed: expected: %s, got: %s" % (expected, actual)
 
-def test_invert_words():
-	test (invert_words('I am a cat'), 'cat a am I')
-	test (invert_words('I am a cat'), 'I ma a tac')
-	test (invert_words(''), '')
+#def test_invert_words():
+	#test (invert_words('I am a cat'), 'cat a am I')
+	#test (invert_words('I am a cat'), 'I ma a tac')
+	#test (invert_words(''), '')
 	
 	
-test_invert_words()
+#test_invert_words()
+
+invert_letters('123456')
