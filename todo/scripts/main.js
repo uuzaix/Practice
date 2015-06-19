e9324dbae5c31959bd3b0896cf355a9566ef3d40
Larisa Dolgova <larisa.dolgova@gmail.com>
2015-06-20 00:00:51 +0300

    Add localStorage

--- todo/scripts/main.js
+++ todo/scripts/main.js
@@ -1,28 +1,37 @@
 var addButton =  document.getElementById('button');
 var list = document.getElementById('todo-list');
+var count = 0;
 
 function addToList() {
 
+	count++;
 	var input = document.getElementById('input');
 	var li = document.createElement('li');
+	var checkBox = document.createElement("input");
+	var deleteButton = document.createElement("input");
 
 	if (input.value == '') {
 		return;
 	}
-	var checkBox = document.createElement("input");
+
+	li.id = count + '_li';
+
 	checkBox.type = 'checkbox';
 	checkBox.checked = false;
 	checkBox.style.margin = '5px';
-	li.appendChild(checkBox);
-
-	li.appendChild(document.createTextNode(input.value));
-	list.appendChild(li);
-
-	var deleteButton = document.createElement("input");
+	
 	deleteButton.type = 'submit';
 	deleteButton.value = "Delete";
 	deleteButton.style.margin = '5px 15px';
+
+	li.appendChild(checkBox);
+	li.appendChild(document.createTextNode(input.value));
 	li.appendChild(deleteButton);
+
+
+	list.appendChild(li);
+
+	localStorage.setItem(li.id,[input.value, checkBox.checked]);
 
 	checkBox.onclick = function() {
 		if (checkBox.checked) {
@@ -35,9 +44,10 @@ function addToList() {
 
 	deleteButton.onclick = function() {
 		li.parentNode.removeChild(li);
+		localStorage.removeItem(li.id);
 	}
 
-	input.value = null;
+	input.value = '';
 }
 
 
