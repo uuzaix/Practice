7f23ba8a78deec0a930fec5c20504163963d5a7f
Larisa Dolgova <larisa.dolgova@gmail.com>
2015-06-22 23:28:02 +0300

    Improved localStorage usage

--- todo/scripts/main.js
+++ todo/scripts/main.js
@@ -1,13 +1,16 @@
 var addButton =  document.getElementById('button');
 var list = document.getElementById('todo-list');
-var count = 1;
-//count++;
 var storageLength = localStorage.length;
 var newInput;
+var maxId;
+
+if (localStorage.length == 0) {
+	localStorage.setItem('max', 0);
+}
+
+var maxStorageId = parseInt(localStorage.getItem('max'));
 
 function addToList(newId, addInput, checkedStatus) {
-	count++;
-
 	var li = document.createElement('li');
 	var checkBox = document.createElement("input");
 	var deleteButton = document.createElement("input");
@@ -30,26 +33,22 @@ function addToList(newId, addInput, checkedStatus) {
 	li.appendChild(document.createTextNode(addInput));
 	li.appendChild(deleteButton);
 
-
 	list.appendChild(li);
 
 	if (checkBox.checked) {
-			li.setAttribute("class", "lineThrough");
-		}
+		li.setAttribute("class", "lineThrough");
+	}
 
 	function writeStorage() {
 		localStorage.setItem(li.id,JSON.stringify([addInput, checkBox.checked]));
+		localStorage.setItem('max',newId);
 	}
 
 	writeStorage();
 
-	function readStorage() {
+	function updateStorage() {
 		currentStateString = localStorage.getItem(li.id);
 		currentStateList = JSON.parse(currentStateString);
-	}
-
-	function updateStorage(){
-		readStorage();
 		currentStateList = [currentStateList[0], checkBox.checked];
 		localStorage.setItem(li.id,JSON.stringify(currentStateList));
 	}
@@ -68,37 +67,32 @@ function addToList(newId, addInput, checkedStatus) {
 		li.parentNode.removeChild(li);
 		localStorage.removeItem(li.id);
 	}
-
 	newInput = null;
-	console.log(newInput);
 }
-
 
 addButton.onclick = function() {
 	newInput = document.getElementById('input').value;
-	addToList(count, newInput, false);
+	maxStorageId = parseInt(localStorage.getItem('max'));
+	addToList((maxStorageId +1), newInput, false);
 }
-
-
 
 document.onkeydown = function() {
 	if (window.event.keyCode == '13') {
 		newInput = document.getElementById('input').value;
-		addToList(count, newInput, false);
+		maxStorageId = parseInt(localStorage.getItem('max'));
+		addToList((maxStorageId +1), newInput, false);
 	}
 }
 
-
 window.onload = function() {
-	console.log(storageLength);
-	if (!storageLength == 0){
-		for (var i = 1; i <= storageLength; i++){
+	if (storageLength > 1) {
+		for (var i = 1; i <= maxStorageId; i++) {
 			var storageKey = i + '_li';
 			var savedInputString = localStorage.getItem(storageKey);
 			var savedInputList = JSON.parse(savedInputString);
-			var savedInput = savedInputList[0];
-
-			addToList(i, savedInput, savedInputList[1]);
+			if (savedInputList) {
+				addToList(i, savedInputList[0], savedInputList[1]);
+			}
 		}
 	}
 }
