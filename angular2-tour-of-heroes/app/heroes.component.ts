0e8a43e9074cf65d4f933fd3a17866557716d9ed
Larisa Dolgova <larisa.dolgova@gmail.com>
2016-06-06 00:05:18 +0200

    Add http service

--- angular2-tour-of-heroes/app/heroes.component.ts
+++ angular2-tour-of-heroes/app/heroes.component.ts
@@ -2,29 +2,61 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router-deprecated';
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
+import { HeroDetailComponent } from './hero-detail.component';
 
 @Component({
   selector: 'my-heroes',
   templateUrl: 'app/heroes.component.html',
-  styleUrls:  ['app/heroes.component.css']
+  styleUrls:  ['app/heroes.component.css'],
+  directives: [HeroDetailComponent]
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
   selectedHero: Hero;
+  addingHero = false;
+   error: any;
 
   constructor(
     private router: Router, 
     private heroService: HeroService) { }
 
   getHeroes() {
-    this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
+    this.heroService
+        .getHeroes()
+        .then(heroes => this.heroes = heroes)
+        .catch(error => this.error = error); // TODO: Display error message
   }
 
   ngOnInit() {
     this.getHeroes();
   }
-  onSelect(hero: Hero) { this.selectedHero = hero; 
+
+  onSelect(hero: Hero) {
+    this.selectedHero = hero;
+    this.addingHero = false;
   }
+
+  addHero() {
+    this.addingHero = true;
+    this.selectedHero = null;
+  }
+
+  close(savedHero: Hero) {
+    this.addingHero = false;
+    if (savedHero) { this.getHeroes(); }
+  }
+
+  delete(hero: Hero, event: any) {
+    event.stopPropagation();
+    this.heroService
+        .delete(hero)
+        .then(res => {
+          this.heroes = this.heroes.filter(h => h !== hero);
+          if (this.selectedHero === hero) { this.selectedHero = null; }
+        })
+        .catch(error => this.error = error); // TODO: Display error message
+  }
+
   gotoDetail() {
     this.router.navigate(['HeroDetail', { id: this.selectedHero.id }]);
   }
--- angular2-tour-of-heroes/app/main.ts
+++ angular2-tour-of-heroes/app/main.ts
@@ -1,5 +1,17 @@
-import { bootstrap } from '@angular/platform-browser-dynamic';
+import { provide }    from '@angular/core';
+import { XHRBackend } from '@angular/http';
 
+import { InMemoryBackendService, SEED_DATA } from 'angular2-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
+import { bootstrap } from '@angular/platform-browser-dynamic';
+import { ROUTER_PROVIDERS } from '@angular/router-deprecated';
+import { HTTP_PROVIDERS } from '@angular/http';
 import { AppComponent } from './app.component';
 
-bootstrap(AppComponent);
+bootstrap(AppComponent, [
+  ROUTER_PROVIDERS,
+  HTTP_PROVIDERS,
+  provide(XHRBackend, { useClass: InMemoryBackendService }), // in-mem server
+  provide(SEED_DATA, { useClass: InMemoryDataService }) // in-mem server data
+]);
