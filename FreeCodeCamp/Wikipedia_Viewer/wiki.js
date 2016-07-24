2ce50283212f684ca29e0841c2e523150bf8855f
Larisa Dolgova <larisa.dolgova@gmail.com>
2016-07-24 23:40:04 +0200

    Add basics of WikiViewer with get_random_article button

---
+++ FreeCodeCamp/Wikipedia_Viewer/wiki.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+  $(".wiki-btn").click(function(){
+    var url = "https://en.wikipedia.org/wiki/Special:Random";
+    var win = window.open("https://en.wikipedia.org/wiki/Special:Random", '_blank');
+    if (win) {
+      console.log("aaa");
+      win.focus();
+    }
+    else {
+      alert('Please allow popups for this website');
+    }
+  });
+}
