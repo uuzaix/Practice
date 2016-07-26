45a1a33fd8de166f5ab60127b7ac542d57b3adbe
Larisa Dolgova <larisa.dolgova@gmail.com>
2016-07-26 23:40:17 +0200

    Add request to wiki api

--- FreeCodeCamp/Wikipedia_Viewer/wiki.js
+++ FreeCodeCamp/Wikipedia_Viewer/wiki.js
@@ -1,6 +1,5 @@
 window.onload = function() {
   $(".wiki-btn").click(function(){
-    var url = "https://en.wikipedia.org/wiki/Special:Random";
     var win = window.open("https://en.wikipedia.org/wiki/Special:Random", '_blank');
     if (win) {
       console.log("aaa");
@@ -11,8 +10,23 @@ window.onload = function() {
     }
   });
   $("#form").submit(function() {
-    var dat = $("#wiki-search").val();
-    console.log(dat);
+    var user_input = $("#wiki-search").val();
+    var apiLink = "https://en.wikipedia.org/w/api.php?action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=extracts&rvprop=content&format=json&gsrsearch=";
+    $.ajax({
+      url: apiLink+user_input,
+      dataType: "jsonp",
+      success: function(data){
+      console.log(data.query.pages);
+      var items = [];
+      $.each (data.query.pages, function(key, value) {
+        items.push('<p>' + value.title + '</p><br>');
+      })
+      $("#search-results").html(items.join(""));
+    }
+  });
     return false;
-  })
+  });
+
 }
+
+
