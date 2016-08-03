c700bc18d91be14f3548f239868212c605b2df3b
Larisa Dolgova <larisa.dolgova@gmail.com>
2016-08-03 20:48:49 +0200

    Change to wunderground API due to change in Chrome Geolocation API

--- FreeCodeCamp/Weather/weather.js
+++ FreeCodeCamp/Weather/weather.js
@@ -14,8 +14,7 @@ window.onload = function() {
     lat = startPos.coords.latitude;
     lon = startPos.coords.longitude;
     console.log(lat, lon);
-    url = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&APPID=0e64fd19006ed36b06872f0cb94f4540";
-    console.log(url);
+    url = "https://api.wunderground.com/api/bbb07fbe6c47678a/conditions/q/" + lat + "," + lon + ".json";
     parseWeatherReasponse(url);
   };
   var geoError = function(error) {
@@ -30,48 +29,49 @@ window.onload = function() {
   navigator.geolocation.getCurrentPosition(geoSuccess, geoError, geoOptions);
 
   function parseWeatherReasponse(url) {
-    $.getJSON(url,function(data) {
-      console.log(data.main.pressure);
-      var currentTemp = [Math.round(data.main.temp -273.15), "C"];
-      $(".location").text("Current weather in  " + data.name);
-      $("#temperature").text(currentTemp.join(" \u00B0"));
-      $("#pressure").text("Pressure: " + data.main.pressure +" hPa");
-      // $("#weather-type").text(data.weather[0].main);
-      $("#precipitation").text(data.weather[0].description.charAt(0).toUpperCase()+data.weather[0].description.slice(1));
-      $("#icon").prop('src', "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
-      // $("#icon").text(data.weather[0].icon);
-      chooseBackgroundColor(data.weather[0].icon);
-      $("#temperature").click(function() {
+    $.getJSON(url, function(data) {
+      console.log(data);
+      // console.log(data.current_observation.icon_url);
+      var currentTemp = [data.current_observation.temp_c, "C"];
+      $(".location").text("Current weather in  " + data.current_observation.display_location.full);
+      $("#temperature").html('Temperature: <b><span id="grads"></span></b>');
+      $("#grads").text(currentTemp.join(" \u00B0"));
+      $("#pressure").text("Pressure: " + data.current_observation.pressure_mb +" mB");
+      $("#precipitation").text(data.current_observation.weather);
+      $("#humidity").text("Relative humidity: " + data.current_observation.relative_humidity);
+      $("#icon").prop('src',  "http://icons.wxug.com/i/c/i/" + data.current_observation.icon + ".gif");
+      chooseBackgroundColor(data.current_observation.weather.split(" "));
+      $("#grads").click(function() {
         if (currentTemp[1] === "C") {
-          currentTemp = [Math.round(data.main.temp*9/5 - 459.67), "F"];
+          currentTemp = [data.current_observation.temp_f, "F"];
         }
         else {
-          currentTemp = [Math.round(data.main.temp -273.15), "C"];
+          currentTemp = [data.current_observation.temp_c, "C"];
         }
-          $("#temperature").text(currentTemp.join(" \u00B0"));
+          $("#grads").text(currentTemp.join(" \u00B0"));
         });
       });
     }
-    function chooseBackgroundColor (icon) {
+    function chooseBackgroundColor (weather) {
       var colors = {"sun": "#FED388", "cloud": "#6BC5DF", "rain": "#7B98E1", "storm" : "#947DE3", "snow": "#FFFCEE", "mist": "#9DA8C3", "default": "#57BC90"};
       var color;
-      switch (icon.slice(0,2)) {
-        case "01": case "02":
+      switch (weather[weather.length-1]) {
+        case "Clear": case "Sunny":
           color = colors.sun;
           break;
-        case "03": case "04":
+        case "Cloudy":
           color = colors.cloud;
           break;
-        case "09": case "10":
+        case "Rain": case "Sleet":
           color = colors.rain;
           break;
-        case "11":
+        case "Thunderstorm": case "Flurries":
           color = colors.storm;
           break;
-        case "13":
+        case "Snow":
           color = colors.snow;
           break;
-        case "50":
+        case "Hazy":
           color = colors.mist;
           break;
         default:
