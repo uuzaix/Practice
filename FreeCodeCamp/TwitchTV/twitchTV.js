99d830d0ac1bfc817eb0d994a1bba6ce67472c53
Larisa Dolgova <larisa.dolgova@gmail.com>
2016-08-10 23:36:51 +0200

    Add parse of simple query

--- FreeCodeCamp/TwitchTV/twitchTV.js
+++ FreeCodeCamp/TwitchTV/twitchTV.js
@@ -1,6 +1,13 @@
 window.onload = function() {
-
-$.getJSON('https://api.twitch.tv/kraken/streams/freecodecamp?callback=?', function(data) {
-  console.log(data);
-});
+  $.getJSON('https://api.twitch.tv/kraken/streams/freecodecamp?callback=?', function(data) {
+    console.log(data);
+    $("#user").html("<a href='" + data._links.channel + "'>FreecodeCamp</a>");
+    if (data.stream === null) {
+      $("#status").text(" is offline");
+    }
+    else {
+      $("#status").text(" is online");
+    }
+    
+  });
 }
