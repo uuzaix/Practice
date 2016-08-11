964876be1751f46ce5deb84344747ecac73e491d
Larisa Dolgova <larisa.dolgova@gmail.com>
2016-08-11 22:35:06 +0200

    Add request for provided channels

--- FreeCodeCamp/TwitchTV/twitchTV.js
+++ FreeCodeCamp/TwitchTV/twitchTV.js
@@ -1,13 +1,21 @@
-window.onload = function() {
-  $.getJSON('https://api.twitch.tv/kraken/streams/freecodecamp?callback=?', function(data) {
-    console.log(data);
-    $("#user").html("<a href='" + data._links.channel + "'>FreecodeCamp</a>");
-    if (data.stream === null) {
-      $("#status").text(" is offline");
-    }
-    else {
-      $("#status").text(" is online");
-    }
-    
+var channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+
+function getChannelsInfo () {
+  channels.forEach(function(channel) {
+    $.getJSON('https://api.twitch.tv/kraken/streams/' + channel + '?callback=?', function(data) {
+      console.log(data);
+      var status;
+      if (data.stream === null) {
+        status = "offline";
+      }
+      else {
+        status = data.stream.game;
+      }
+      $("#channels-list").append('<li><a href="' + data._links.channel + '">' + channel + '</a> - ' + status +' </li>');
+    });
   });
+}
+
+window.onload = function() {
+  getChannelsInfo();
 }
