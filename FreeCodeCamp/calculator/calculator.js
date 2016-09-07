34ba020eac6778b6d9f887abfd6eac22a925783f
Larisa Dolgova <larisa.dolgova@gmail.com>
2016-09-08 00:15:41 +0200

    Separate format_result function

--- FreeCodeCamp/calculator/calculator.js
+++ FreeCodeCamp/calculator/calculator.js
@@ -41,6 +41,9 @@ window.onload = function() {
           inputToDisplay.push(operator);
         }
         if (currValue.length !== 0) {
+          if (currValue[currValue.length-1] === ".") {
+            currValue.pop();
+          }
           inputToDisplay.push(currValue.join(""));
           inputToDisplay.push(operator);
           currResult = calculate(currValue, currOperator, currResult);
@@ -93,7 +96,6 @@ function calculate (value, currOperator, currResult) {
   }
   else if (currOperator === "\xF7") {
     currResult /= parseFloat(value.join(""));
-    console.log("divide ", currResult);
   }
   else if (currOperator === "\xD7") {
     currResult *= parseFloat(value.join(""));
@@ -110,23 +112,10 @@ function calculate (value, currOperator, currResult) {
       inputToDisplay.push(currValue.join(""));
       inputToDisplay.push($(this).text());
       $("#all-input").text(inputToDisplay.join(""));
-      console.log("before equal ", currValue, currOperator, currResult);
+      // console.log("before equal ", currValue, currOperator, currResult);
       currResult = calculate(currValue, currOperator, currResult);
-      console.log("equal ", currResult);
-      result = currResult;
-      var result_string = result.toString();
-      if (result_string.length > 8) {
-        if (result_string.indexOf(".") !== -1 && result_string.indexOf(".") < 8) {
-          result = result.toFixed(7-result_string.indexOf("."));
-          console.log("tofixed ", result);
-          while (result.endsWith("0")) {
-            result = result.substring(0, result.length-2);
-          }
-        }
-        else {
-          result = "too big";
-        }
-      }
+      // console.log("equal ", currResult);
+      result = formatResult(currResult);
       $("#result").text(result);
       currValue = [];
       currOperator = "+";
@@ -140,7 +129,7 @@ function calculate (value, currOperator, currResult) {
 var operators =["-", "+", "\xD7", "\xF7"];
 
 // returns true if last input char is operator, if digit - false
-function checkLastCharIsOperator (input) {
+function checkLastCharIsOperator(input) {
   if (operators.includes(input[input.length - 1])) {
     return true;
   }
@@ -148,10 +137,21 @@ function checkLastCharIsOperator (input) {
     return false;
   }
 }
-//checks if input contains at least one operator
-function checkInputIncludesOperator(input) {
-  var givenOperators = operators.filter(function(op) {
-    return input.includes(op)
-  });
-  return givenOperators.length > 0 ? true : false;
+
+//format the result to fit in display
+function formatResult(result) {
+  var result_string = result.toString();
+  if (result_string.length > 8) {
+    if (result_string.indexOf(".") !== -1 && result_string.indexOf(".") < 8) {
+      result = result.toFixed(7-result_string.indexOf("."));
+      console.log("tofixed ", result);
+      while (result.endsWith("0")) {
+        result = result.substring(0, result.length-1);
+      }
+    }
+    else {
+      result = "too big";
+    }
+  }
+  return result;
 }
