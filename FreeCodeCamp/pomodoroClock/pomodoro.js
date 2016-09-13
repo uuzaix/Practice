73ad1372e732d8bf94a75418608fd43c32f3e09a
Larisa Dolgova <larisa.dolgova@gmail.com>
2016-09-13 18:26:56 +0200

    Add buttons to increase/decrease timer time

--- FreeCodeCamp/pomodoroClock/pomodoro.js
+++ FreeCodeCamp/pomodoroClock/pomodoro.js
@@ -1,20 +1,46 @@
 window.onload = function() {
   var timeLeft = 10;
+  var pauseTimeLeft = 5;
+  $("#clock").text(timeLeft);
   $("#start").click(function(e) {
     var timeInterval = setInterval(function() {
-      $("#clock").text(timeLeft);
-      timeLeft --;
-      if (timeLeft < 0) {
+      if (timeLeft >= 0) {
+
+        $("#clock").text(timeLeft);
+        timeLeft --;
+      }
+      else {
         clearInterval(timeInterval);
+        var pauseInterval = setInterval(function() {
+          if (pauseTimeLeft >= 0) {
+          $("#clock").text(pauseTimeLeft);
+          pauseTimeLeft --;
+        }
+        else {
+          clearInterval(pauseInterval);
+          }
+        }, 1000);
       }
     }, 1000);
+
     $("#pause").click(function(e) {
       clearInterval(timeInterval);
     });
     $("#stop").click(function(e) {
       clearInterval(timeInterval);
       timeLeft = 10;
-      $("#clock").text(0);
+      $("#clock").text(timeLeft);
     });
   });
+
+  $("#timerUp").click(function(e) {
+    timeLeft ++;
+    $("#clock").text(timeLeft);
+  });
+
+  $("#timerDown").click(function(e) {
+    timeLeft --;
+    $("#clock").text(timeLeft);
+  });
+
 }
