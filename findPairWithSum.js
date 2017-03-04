f83e41a91148b953147b0c52bdbfc1c0a6fbb1ad
Larisa Dolgova <larisa.dolgova@gmail.com>
2017-03-04 23:37:01 +0100

    Find pair with given sum

---
+++ findPairWithSum.js
@@ -0,0 +1,22 @@
+// [1,2,3,9] sum=8  -> 'no pair which sum is equal to sum'
+// [1,2,4,4] sum=8 - > [4, 4]
+
+function findPairWithSum(arr, sum) {
+  minIndex = 0;
+  maxIndex = arr.length - 1;
+
+  while (minIndex !== maxIndex) {
+    let tempSum = arr[minIndex] + arr[maxIndex];
+    if (tempSum === sum) {
+      return [arr[minIndex], arr[maxIndex]]
+    } else if (tempSum > sum) {
+      maxIndex--;
+    } else {
+      minIndex++;
+    }
+  }
+  return 'no pair'
+}
+
+console.log(findPair([1, 2, 3, 9], 8))
+console.log(findPair([1, 2, 4, 4], 8))
